95e4923e82604126f7af0a742c69b9ff4032a989
Josep Vidal <josepvidalvidal@gmail.com>
2020-11-21 19:04:17 +0100

    🚘  Now its working with Parcel 2 nightly version

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## Parcel 1 or 2 and Tailwind 2 setup with Preact and TypeScript
 As almost 2021, most of current tutorials and guides on the internet of how to set up this combination are outdated. More so considering the latest iterations of Tailwind and Parcel that bumps the PostCSS version in use.
 
+To use Parcel 1, just downgrade the version of "parcel", in ``package.json`` to one, works exactly the same.
  
--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,11 @@ const App = () => {
         <div className="container text-center mx-auto bg-yellow-100 rounded-lg my-8 py-12">
             <h1 className="text-5xl mb-12">Hello from Parcel and Tailwind!</h1>
             <p className="text-xl mb-12 w-3/6 mx-auto">
-                This project has <b>Parcel ^1.X.X</b> with <b>Tailwind ^2.X.X</b> working. Thanks to :global in ./index.pcss you can use
+                This project has <b>Parcel ^2.X.X</b> with <b>Tailwind ^2.X.X</b> working. Thanks to :global in ./index.pcss you can use
                 tailwind classes everywhere, and thanks to "css-modules" you also can use .pcss locally and import
                 the styles per file.
             </p>
-            <Button>Hover me or make the page smaller!</Button>
+            <Button>Hover me, click me and leave area or make the page smaller!</Button>
         </div>
     )
 }
